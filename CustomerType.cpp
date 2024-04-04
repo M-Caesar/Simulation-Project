d1526de44c201b07bb89b506661c2a4e5a62bf79
NICHOLAS AUGUSTUS <nicholasaugustus1@gmail.com>
2024-04-04 19:40:28 -0400

    Finished implementing functions. Next step is to begin client program

--- CustomerType.cpp
+++ CustomerType.cpp
@@ -64,3 +64,119 @@ void ServerType::setTranTime()
 	time = currCustomer.getTranTime();
 	tranTime = time;
 }
+void ServerType::decreaseTranTime()
+{
+	tranTime--;
+}
+int ServerType::getRemainingTransTime()
+{
+	return tranTime;
+}
+void ServerType::setCurrCust(CustomerType customer)
+{
+	currCustomer = customer;
+}
+int ServerType::getCurrCustID()
+{
+	return currCustomer.GetCustID();
+}
+int ServerType::getCurrCustArrTime()
+{
+	return currCustomer.getArrTime();
+}
+int ServerType::getCurrCustWaitTime()
+{
+	return currCustomer.getWaitTime();
+}
+int ServerType::getCurrCustTranTime()
+{
+	return currCustomer.getTranTime();
+}
+
+//ServerListType functions
+
+serverListType::serverListType(int nums)
+{
+	numofServs = nums;
+	servs = new ServerType[nums];
+}
+
+serverListType::~serverListType()
+{
+	delete[] servs;
+}
+
+int serverListType::getFreeServID()
+{
+	int servID = -1;
+
+	for (int i = 0; i < numofServs; i++)
+	{
+		if (servs[i].isFree())
+		{
+			servID = i;
+			break;
+		}
+		return servID;
+	}
+}
+
+int serverListType::getNumofBusyServ()
+{
+	int busyServs = 0;
+	for (int i = 0; i < numofServs; i++)
+		if (!servs[i].isFree())
+			busyServs++;
+	return busyServs;
+}
+
+void serverListType::setServBusy(int servID, CustomerType customer, int time)
+{
+	servs[servID].setBusy();
+	servs[servID].setTranTime(time);
+	servs[servID].setCurrCust(customer);
+}
+
+void serverListType::updateServ(ostream& outFile)
+{
+	for (int i = 0; i < numofServs; i++)
+		if (!servs[i].isFree())
+		{
+			servs[i].decreaseTranTime();
+			if (servs[i].getRemainingTransTime() == 0)
+			{
+				outFile << "From server number " << (i + 1)
+					<< " customer number"
+					<< servs[i].getCurrCustID()
+					<< "\n		departed at time unit"
+					<< servs[i].getCurrCustArrTime() +
+					servs[i].getCurrCustWaitTime() +
+					servs[i].getCurrCustTranTime() << endl;
+				servs[i].setFree();
+			}
+		}
+}
+
+//waitQueue
+
+waitingCustomerQueueType::waitingCustomerQueueType(int size) :queueType<CustomerType>(size)
+{
+	CustomerType customer;
+
+	customer.setWaitTime(-1);
+	int wTime = 0;
+
+	addQueue(customer);
+
+	while (wTime != -1)
+	{
+		customer = front();
+		deleteQueue();
+
+		wTime = customer.getWaitTime();
+		if (wTime == -1)
+			break;
+		customer.incWaitTime();
+		addQueue(customer);
+	}
+}
